43a8df38b8ec0ec66cfaa9638fc5b644f63019b2
Josep López <jayden.haylie@gmail.com>
2018-07-19 17:55:57 -0400

    Added validation

--- src/add.js
+++ src/add.js
@@ -2,9 +2,24 @@ const electron = require('electron');
 const { ipcRenderer } = electron;
 
 const form = document.querySelector('form');
+const input = form.children[1];
+const btn = form.children[2];
+
+function checkInputValue() {
+  if (input.value === '') {
+    btn.disable = true;
+    return false;
+  } else {
+    btn.disable = false;
+    input.style.border = '2px solid green';
+  }
+}
+
+checkInputValue();
 
 form.addEventListener('submit', (e) => {
   const item = e.target.elements.add.value;
+
   ipcRenderer.send('item:add', item);
   e.preventDefault();
 });
